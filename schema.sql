7668356ca7d67ee716f5206d1bb6e385dcdc2fca
Simon Lindblad <simon.lindblad93@gmail.com>
2016-05-29 22:44:32 +0200

    Update order in schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,10 @@
+-- A table that stores all the lists in the system.
+drop table if exists list cascade;
+create table list(
+    id serial primary key,
+    name text not null
+);
+
 -- A table that stores all the tasks in the system.
 drop table if exists task cascade;
 create table task(
@@ -5,11 +12,4 @@ create table task(
     name text not null,
     done boolean default false,
     list int references list(id)
-);
-
--- A table that stores all the lists in the system.
-drop table if exists list cascade;
-create table list(
-    id serial primary key,
-    name text not null
 );
