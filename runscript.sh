d519bcf4f28e8d3daf02a5a5bfd7b73a3e48967e
marjo688 <markusjohansson1994@hotmail.com>
2018-02-16 13:27:48 +0100

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -1,19 +1,19 @@
 
-docker build -t tdde06_go Go
-
-docker build -t tdde06_pg PostgresSQL
-
-docker build -t tdde06_nginx NGINX
-
 docker stop pgserver || true && docker rm pgserver || true
-docker stop go_server || true && docker rm go_server || true
+docker stop goserver || true && docker rm go_server || true
 
-docker run --name pgserver -it -d tdde06_pg
+docker build -t go Go
+
+docker build -t pg PostgresSQL
+
+docker build -t nginx NGINX
+
+docker run --name pgserver -it -d pg
 
 sleep 5
 
-docker run --name go_server --link pgserver:db tdde06_go go test
+docker run --name goserver --link pgserver:db go go test
 
 docker stop pgserver
 docker rm pgserver
-docker rm go_server
+docker rm goserver
