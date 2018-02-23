6d2ecdedd79ea6546dd195fd953e93b88cf04e03
marjo688 <markusjohansson1994@hotmail.com>
2018-02-23 09:16:50 +0100

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -13,7 +13,9 @@ docker run --name pgserver -it -d pg
 
 sleep 5
 
-docker run --name goserver --link pgserver:db go go test
+docker run --name goserver --link pgserver:db go
+
+go test
 
 sleep 5
 
